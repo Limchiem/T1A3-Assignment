b107d2bbd548e2711c0ba10cad8e0fca3833d77f
Limchiem <jinkomaster@hotmail.com>
2023-05-05 23:22:31 +1000

    added pin input

--- src/main.py
+++ src/main.py
@@ -4,60 +4,38 @@ print("\t***************************")
 print("\t***  Welcome To CA ATM  ***")
 print("\t***************************")
 
-# print("Press enter to continue")
-# acc_holder = acc_name()
-
-# acc_name = ["Simon, Mary, John, Lily"]
-
-# acc_id = ""
-
-# while True:
-#     try:
-#         acc_id = input("What is your name?")
-#         acc_found = [account for account in acc_name if acc_name == acc_id]
-#         if(len(acc_found) > 0):
-#             acc_holder = acc_found[0]
-#             break
-#         else:
-#             print("Account not found")
-#     except:
-#         print("Account not found")
-
-# def user_menu():
-#     print ("Please choose an option:")
-#     print ("1. Withdraw")
-#     print ("2. Deposit")
-#     print ("3. Check balance")
-#     print ("4. Exit")
-
-# password = 1111
 user_choice = 0
 balance = 5000
-
-# pin = int(input("Enter your pin: "))
-
-# if pin == password:
-
 current_user = atm_user("","")
 
 list_of_user = []
 
-list_of_user.append(atm_user("simon","1234"))
-list_of_user.append(atm_user("mary","1234"))
+list_of_user.append(atm_user("simon", 1234))
+list_of_user.append(atm_user("mary", 1234))
 
 loginId = ""
 
 while True:
     try:
-        loginId = str(input("What is your log in ID?: ").strip().lower())
+        loginId = str(input("Please enter your log in ID: ").strip().lower())
         Id_match = [id for id in list_of_user if id.userId == loginId]
         if(len(Id_match) > 0):
-            current_user = Id_match
+            current_user = Id_match[0]
             break
         else:
             print("Incorrect User ID. Please try again")
     except:
         print("Incorrect User ID. Please try again")
+
+while True:
+    try:
+        pin = int(input("Please enter your pin: ").strip())
+        if(current_user.get_userPin() == pin):
+            break
+        else:
+            print("Incorrect PIN. Please try again.")
+    except:
+        print("Incorrect PIN. Please try again.")
 
 while user_choice !=4:
     print ("Please choose an option:")
@@ -90,6 +68,3 @@ while user_choice !=4:
 
     elif user_choice >= 5:
         print ("invalid input")
-
-# else:
-#     print("Incorrect pin")
