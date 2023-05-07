89703b83eceb3690d83ec9d7aa5792e35fd5ac6d
Limchiem <jinkomaster@hotmail.com>
2023-05-07 19:29:37 +1000

    added extra function to check if venv exist

--- src/run1.sh
+++ src/run1.sh
@@ -3,3 +3,10 @@
 [[ "$(python3 -V)" =~ "Python 3" ]] && echo "Python 3 is installed"
 
 echo "go to https://www.python.org/downloads/ if not installed."
+
+# check if venv exist
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "No VIRTUAL_ENV set"
+else
+    echo "VIRTUAL_ENV is set"
+fi
